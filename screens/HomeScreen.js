55f84a2eee06b5c1db33b46f62894f0bbe3b7286
Frank Santos <fransecre@gmail.com>
2023-04-04 16:52:45 +0200

    init Category Cards

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,14 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from 'react-native-heroicons/outline'
+import Categories from '../components/Categories';
+
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -12,19 +20,56 @@ const HomeScreen = () => {
   }, [])
 
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView className="bg-white pt-5">
+      
+      {/* Header */ }
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
         <Image  source={{
           uri: 'https://links.papareact.com/wru'
         }}
         className="h-7 w-7 bg-gray-300 p-4 rounded-full"
         />
-      </View>
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-red-500">HomeScreen Text</Text>
-    </View>
-    </SafeAreaView>
-  )
-}
+      
 
-export default HomeScreen
+    <View className="flex-1">
+      <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+      <Text className="font-bold text-xl">Current Location
+        <ChevronDownIcon size={20} color="#00CCBB" />
+      </Text>
+    </View>
+
+    <UserIcon size={35} color="#00CCBB" />
+    </View>
+
+    {/* Search Bar */ }
+    <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+      <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+        <MagnifyingGlassIcon  color="gray" size={20} />
+        <TextInput 
+        placeholder="Restaurants"
+        keyboardType="default"
+        />
+      </View>
+
+      <AdjustmentsVerticalIcon color="#00CCBB" />
+      </View>
+
+      {/* Contenido Scroll */ }
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{ 
+          paddingBottom: 100,
+         }}
+          >
+          {/* Categorías */ }
+          <Categories />
+
+          {/* Destacados */ }
+      </ScrollView>
+  
+
+  </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
