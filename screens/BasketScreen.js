9727259b0cd2dcc52db5c6dafce63ccce0a898e5
Frank Santos <fransecre@gmail.com>
2023-04-08 22:38:38 +0200

    Basket Screen

---
+++ screens/BasketScreen.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useDispatch, useSelector } from 'react-redux'
+import {selectBasketItems} from '../features/basketSlice'
+
+const BasketScreen = () => {
+    const navigation = useNavigation()
+    const restaurant = useSelector(selectRestaurant)
+    const items = useSelector(selectBasketItems)
+    const dispatch = useDispatch()
+
+  return (
+    <View>
+      <Text>BasketScreen</Text>
+    </View>
+  )
+}
+
+export default BasketScreen
