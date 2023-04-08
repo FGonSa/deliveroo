ce272dfa4dcc4d405a7f88964e651ffb221d136b
Frank Santos <fransecre@gmail.com>
2023-04-08 17:57:32 +0200

    Added Add and Remove from Basket

--- features/basketSlice.js
+++ features/basketSlice.js
@@ -16,7 +16,16 @@ export const basketSlice = createSlice({
       state.items = [...state.items, action.payload]
     },
     removeFromBasket: (state, action) => {
-      state.items -= 1
+      const index = state.items.findIndex((item) => item.id === action.payload.id)
+
+      let newBasket = [...state.items]
+
+      if(index >=0){
+        newBasket.splice(index,1)
+      }else{
+        console.warn(`Can't remove product (id: ${action.payload.id}) as its not in basket!`)
+      }
+      state.items = newBasket;
     },
   },
 })
@@ -24,6 +33,9 @@ export const basketSlice = createSlice({
 // Action creators are generated for each case reducer function
 export const { addToBasket, removeFromBasket } = basketSlice.actions
 
-export const selectBaskenItems = (state) => state.basket.items;
+export const selectBasketItems = (state) => state.basket.items;
+
+export const selectBasketItemsWithId = (state, id) =>
+  state.basket.items.filter((item) => item.id === id)
 
 export default basketSlice.reducer
