7783bc866fd238fb115c0c9152b178b76dfebe77
Frank Santos <fransecre@gmail.com>
2023-04-09 02:05:28 +0200

    Added Animations

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import RestaurantScreen from './screens/RestaurantScreen'
 import { store } from './store'
 import { Provider } from 'react-redux'
 import BasketScreen from './screens/BasketScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +25,9 @@ export default function App() {
         name="Basket" 
         component={BasketScreen} 
         options ={{ presentation: "modal", headerShown: false }}
+        />
+        <Stack.Screen name="PreparingOrderScreen" component={PreparingOrderScreen} 
+        options={{ presentation: "fullScreenModal", headerShown: false }}
         />
       </Stack.Navigator>
     </TailwindProvider>
